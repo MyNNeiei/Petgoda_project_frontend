0fd281fe4fb637d4b37390a6cb537e462bd72b49
MyNNeiei <kullananzaza@gmail.com>
2025-02-20 00:17:42 +0700

    next station profile

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -75,18 +75,18 @@ export default function Home() {
         <div className="my-20 px-20">
           <h1 className="text-5xl font-bold text-start">Recommend</h1>
           <div className="my-10">
-            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-[#2A1D16] dark:border-[#2A1D16]">
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-[#2A1D16] dark:border-[#2A1D16]">
               <a href="#">
-                <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
               </a>
               <div class="p-5">
                 <a href="#">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
                 </a>
-                <p class="mb-3 font-normal text-[#886551]">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <p className="mb-3 font-normal text-[#886551]">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
                 <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-[#886551] bg-[#D2C8BC] rounded-lg hover:bg-[#D2C8BC] focus:ring-4 focus:outline-none">
-                  Read more
-                  <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                  See more
+                  <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                   </svg>
                 </a>
